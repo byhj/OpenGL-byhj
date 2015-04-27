f005daa5fc9f17090c1b3e48f625afd775f85e9a
binyu <binyuapple@163.com>
2015-04-27 22:30:16 +0800

    Add Framework

---
+++ Framework/common.h
@@ -0,0 +1,32 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#include "shader.h"
+#include "glException.h"
+
+//gl header
+#include <gl/glew.h>
+#include <gl/glfw3.h>
+#include <gl/freeglut.h>
+
+
+//texture header
+#include <SOIL/SOIL.h>
+#include <assimp/Importer.hpp>
+
+//math header
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_access.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "byhj.h"
+
+//link the lib
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "SOIL.lib")
+#pragma comment(lib, "assimp.lib")
+
+#endif //  COMMON_H
